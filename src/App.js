c9478d192c5e05ce141f2595bf6aebc0a81b3d33
HSH-85 <gjtjdgns@gmail.com>
2025-01-20 12:12:14 +0900

    blog 끝

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Modal from "./Modal";
 
 function App() {
   // State 정의 시작
+  // 입력 상자 글 받아서 저장하는 state
+  const [inputValue, setinputValue] = useState('');
   let post = "첫 블로그 글";
+  const [contentValue, seTcontentValue] = useState('');
 
-   // Modal 출력용 state
-   let [currentIndex, setCurrentIndex] = useState(0);
+  // Modal 출력용 state
+  let [currentIndex, setCurrentIndex] = useState(0);
 
   // 타이틀 스테이트 생성
   let [title, setTitle] = useState([
@@ -34,7 +37,6 @@ function App() {
 
   // 좋아요 값 증가용 state
   let [like, setLike] = useState([0, 0, 0]);
- 
 
   function addLike(num) {
     // 1.좋아요 배열을 복사
@@ -104,30 +106,123 @@ function App() {
 
       {title.map((x, index) => {
         return (
-          <div className="list">
+          <div className="list" ket={index}>
             <h4
               onClick={() => {
                 // 현재 선택한 인덱스를 state에 저장
                 setCurrentIndex(index);
-                if (currentIndex != index){
+                if (currentIndex != index) {
                   setShowModal(true);
-                } else if(currentIndex == index && showModal == false){
+                } else if (currentIndex == index && showModal == false) {
                   setShowModal(true);
-                }else{
+                } else {
                   setShowModal(false);
                 }
               }}
             >
               {title[index]}
-              <span onClick={(e) => {
-                e.stopPropagation();
-                addLike(index)}}>👍</span>
+              <span
+                onClick={(e) => {
+                  e.stopPropagation();
+                  addLike(index);
+                }}
+              >
+                👍
+              </span>
               {like[index]}
+              <span
+                onClick={(e)=>{
+                  e.stopPropagation();
+
+                  let tempTitle = [...title];
+                  let tempLike=[...like];
+                  let tempDate=[...createDate]
+                  let tempContent=[...content];
+
+                  tempTitle.splice(index, 1);
+                  tempLike.splice(index, 1);
+                  tempDate.splice(index, 1);
+                  tempContent.splice(index, 1);
+
+                  setTitle([...tempTitle]);
+                  setLike([...tempLike]);
+                  setCreateDate([...tempDate]);
+                  setContent([...tempContent]);
+                }}>
+                &nbsp; 🗑
+              </span>
             </h4>
             <p>작성일 : {createDate[index]}</p>
           </div>
         );
+        
       })}
+
+      {/* 입력상자 만들고 입력받기 */}
+      <div>
+        <div>
+        <span>추가할 제목 입력</span>
+        <input id='title' onChange={(e)=>{
+          setinputValue(e.target.value)
+        }}></input>
+        </div>
+        <div>
+        <span>추가할 내용 입력</span>
+        <input id='content' onChange={(e)=>{
+          seTcontentValue(e.target.value)
+        }}></input>
+        </div>
+
+        <button onClick={()=>{
+          // inputValue의 값을 확인 후 배열에 추가
+          if(document.querySelector('#title').value==''){
+            alert('글을 제목을 입력하세요');
+            document.querySelector('#title').focus();
+            return;
+          }else if(document.querySelector('input').value==''){
+            alert('글을 내용을 입력하세요');
+            document.querySelector('#content').focus();
+            return;
+          }
+
+          //inputValue --> title 배열에 글을 추가
+          let temp=[...title];
+          temp.unshift(inputValue);
+          setTitle([...temp]);
+
+
+          //inputValue --> title 배열에 글을 추가
+          let tempContent=[...content];
+          tempContent.unshift(contentValue);
+          setContent([...tempContent]);
+          
+
+          // 좋아요. 빈 배열 하나 추가 0으로 셋팅
+          let tempLike = [...like];
+          tempLike.unshift(0);
+          setLike([...tempLike]);
+
+          // 오늘 날짜 찾아서 
+          let today = new Date();
+          let year = today.getFullYear();
+          let month = today.getMonth()+1;
+          let day = today.getDate();
+          let dateString=year+"년 " +month +"월 "+day+'일';
+          console.log(dateString)
+
+          let tempDate = [...createDate];
+          tempDate.unshift(dateString);
+          setCreateDate([...tempDate]);
+
+          // content 내용 입력받아서 추가로 처리하기
+          // 생성하고 난 뒤 입력칸 빈 칸으로 만들기
+          document.querySelector('#title').value=''
+          document.querySelector('#content').value=''
+          
+
+        }}>글 생성</button>
+      </div>
+
       {/* 리스트 시작 */}
       {/* <div className="list">
         <h4 onClick={()=>{
@@ -175,18 +270,19 @@ function App() {
         // 삼항연산자는 쓸 수 있음.
         // 자식 콤포넌트에 전달할 props를
         // 기술
-        showModal == true ? 
-          <Modal title={title} 
-              createDate={createDate} 
-              content={content}
-              index={currentIndex} 
-              color="yellow"/> 
-          : null
+        showModal == true ? (
+          <Modal
+            title={title}
+            createDate={createDate}
+            content={content}
+            index={currentIndex}
+            color="yellow"
+          />
+        ) : null
       }
       {/* 상세 페이지 종료 */}
     </div>
   );
 }
-
 
 export default App;
